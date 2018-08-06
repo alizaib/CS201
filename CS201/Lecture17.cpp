320ba4511a80056efaec754a7e121189e7ef80e8
Ali Zaib <ali.zaib@madyan.io>
2018-08-06 21:36:50 +0500

    Initial checkin

---
+++ CS201/Lecture17.cpp
@@ -0,0 +1,8 @@
+#include "Lecture17.h"
+#include <iostream>
+using namespace std;
+
+void Test17()
+{
+	cout << "This is Test17" << endl;
+}
---
+++ CS201/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Lecture17.h"
+using namespace std;
+
+void main()
+{
+	Test17();
+	cout << "Hello World";
+}
