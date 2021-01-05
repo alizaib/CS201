8d9e28f27cf4f052e3913dbe4e7362cac0125517
alizaib <alizaib@gmail.com>
2021-01-05 21:56:14 +0500

    some more play with pointers

--- CS201/Lecture17.cpp
+++ CS201/Lecture17.cpp
@@ -5,11 +5,93 @@
 using namespace std;
 
 void EntryPoint17() {
-	//Misc17();
+	Misc17();
+	//PlayWithConstPointers();
+	//PlayWithIntPointer();
 	//ConsoleOutWithCharPointer();
 	//CharAsInt();
-	PrintAllAsciiCharacters();
+	//UsingStrToken();
+	//PrintAllAsciiCharacters();
 	//CountCharAndNumbers();
+}
+
+void Misc17() {
+
+	/*char* name = "Ali Zaib";
+	cout << name << endl; // *name will display first character only, whereas name will display all the string till nullable 0
+	cout << *name << endl;
+	name+=1; //move pointer 
+	cout << *name << endl << name;*/
+
+	/*
+	cout << sizeof(int) << endl;
+	unsigned int num = '7' - '0';
+	unsigned int num2 = '1' - '5';
+	int num3 = '1' - '5';
+	cout << num << "\t" << num2 << "\t" << num3;
+	*/
+
+	/*
+	char zero = '0';
+	char toCheck = '!';
+	unsigned int difference = toCheck - zero;	
+	// an unsigned integer doesn't contain any symbol and the most significant bit is one 
+	// which results in a very large number
+	int diff = toCheck - zero;
+	cout << difference << endl;
+	cout << diff << endl;
+	if (difference > 9)
+		cout << "Not digit";
+	else
+		cout << "digit";
+	*/	
+
+	
+
+	char amount[10] = "46Ali";	
+	int am = Myatoi(amount);
+	cout << am << "\t" << amount << endl;
+	char* number = "253a5";
+	cout << Myatoi(number) << "\t" << number;
+	
+}
+
+void PlayWithConstPointers() {
+	const int a = 3;
+	const int b = 4;
+	int c = 5, d = 9, e = 11;
+
+	int* ptr = &e;
+
+	const int* ptr1 = &d; //read from right to left. ptr1 is a pointer to int const (or const int)
+	int const* ptr2 = &b;  //same as above, ptr2 is a pointer to const int
+	int* const ptr3 = &c;  //ptr3 is a constant pointer to an integer, it cannot be assigned any new value now, ptr3 = &d; will give compiler error
+	const int* const ptr4 = &d; //althoug d is not constant, 
+	//but is assignable to ptr4 and ptr4 will not allow changing contents *ptr4 = 0; is not allowed
+	//*ptr4 = 0;
+
+	 // *ptr1 = 23; //compiler will complaint because ptr1 is a pointer to a constant int and value of that int cannot be changed now
+	//*ptr2 = 23;   //same as above
+
+	cout << *ptr1 << "\t" << *ptr2 << "\t" << *ptr3 << "\t" << *ptr4 << endl;
+}
+
+void PlayWithIntPointer() {
+	int a = 3;
+	int* ptr = &a;
+
+	cout << "&ptr " << &ptr << " is address of the pointer in memory " << endl;
+	cout << "&a " << &a << " is address of variable a in memory" << endl;
+	cout << "*ptr " << *ptr << " is content of the pointer " << endl;
+	cout << "which is equal to a's content " << a << endl;
+	cout << endl << endl;
+	*ptr = 7;
+	cout << "After executing *ptr = 7;" << endl;
+
+	cout << "*ptr " << *ptr << " is content of the pointer " << endl;
+	cout << "which is equal to a's content " << a << endl;
+
+	cout << "size of ptr is " << sizeof(ptr);
 }
 
 void ConsoleOutWithCharPointer() {
@@ -26,70 +108,22 @@ void CharAsInt() {
 	cout << a << endl;
 }
 
-void Misc17() {
-	
-
-	/*char* age = "45.22";
-	cout << age << " " << atoi(age) << " " << atof(age);*/
-
-	
-
-	/*int a = 3;
-	int* ptr = &a;	
-	cout << &ptr << "\t" << *ptr << "\t" << a << endl;
-	*ptr = 7;
-	cout << &a << "\t" << *ptr << "\t" << a << endl;
-	cout << ptr << "\t" << sizeof(ptr);*/
-
-	/*char* name = "Ali Zaib";
-	cout << *name << endl;
-	name+=4;
-	cout << *name << endl << name;*/
-	/*cout << sizeof(int);
-	unsigned int num = '7' - '0';
-	unsigned int num2 = '0' - '1';
-	cout << num << "\t" << num2;*/
-
-	/*char zero = '0';
-	char toCheck = '9';
-	unsigned int difference = toCheck - zero;
-	if (difference > 9)
-		cout << "Not digit";
-	else
-		cout << "digit";
-	*/	
-
-	//const int a = 3;
-	//int b = 5;
-	//const int* ptr = &b;
-	//ptr = &a;
-	////*ptr = 7;
-	//cout << *ptr;
-
-	/*char amount[10] = "46Ali";	
-	int am = Myatoi(amount);
-	cout << am << "\t" << amount;	
-	char* number = "253a5";
-	cout << Myatoi(number) << "\t" << number;*/
-
+void UsingStrToken() {
 	char* statment = "This is a test";
 	int len = strlen(statment);
-	char* statment2 = (char*)malloc((len + 1));
+	char* statment2 = (char*)malloc((static_cast<size_t>(len) + 1));
 	cout << statment2 << endl;
 
 	//Note if you remove the if check, VS gives warning that statement2 could be zero. means mallooc is gaurenteed to return memory.
 	if (statment2 != NULL) {
 		strcpy(statment2, statment);
 	}
-	
-	
+
+
 	char* token = strtok(statment2, " ");
-	do  {
+	do {
 		cout << token << endl;
 	} while ((token = strtok(NULL, " ")) != NULL);
-	
-
-	
 }
 
 void PrintAllAsciiCharacters() {
--- CS201/Lecture17.h
+++ CS201/Lecture17.h
@@ -2,8 +2,11 @@
 void EntryPoint17();
 
 void Misc17();
+void PlayWithConstPointers();
+void PlayWithIntPointer();
 void ConsoleOutWithCharPointer();
 void CharAsInt();
+void UsingStrToken();
 void PrintAllAsciiCharacters();
 void CountCharAndNumbers();
 int Myatoi(char*);
